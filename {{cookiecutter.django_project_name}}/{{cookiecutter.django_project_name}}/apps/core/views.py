3d0c250e682cd3114295a64a38a35beb38a62c34
Gabriel Massadas <5445926+G4brym@users.noreply.github.com>
2020-04-10 10:34:36 +0100

    Fix vuejs view name

--- {{cookiecutter.django_project_name}}/{{cookiecutter.django_project_name}}/apps/core/views.py
+++ {{cookiecutter.django_project_name}}/{{cookiecutter.django_project_name}}/apps/core/views.py
@@ -3,11 +3,11 @@ import json
 from django.views.generic import TemplateView
 
 
-class OpenapiView(TemplateView):
+class VuejsView(TemplateView):
     template_name = "{{cookiecutter.vuejs_project_name}}.html"
 
     def get_context_data(self, **kwargs):
-        context = super(OpenapiView, self).get_context_data()
+        context = super(VuejsView, self).get_context_data()
         context["base_url"] = self.request.build_absolute_uri("/api/v1/")
         context["example"] = json.dumps({"example": 123456})
 
